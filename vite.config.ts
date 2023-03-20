18abf9466139b2484890c33542effb0746e988e8
guandaozhixing <46676762+guandaozhixing@users.noreply.github.com>
2023-03-20 16:56:45 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/guandaozhixing.github.io/',
   plugins: [react()],
   resolve: {
     alias: {
